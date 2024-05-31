ab9fdc96ef36f0cac75ba790477c6923128647c2
okt4hei <142867353+okt4hei@users.noreply.github.com>
2024-05-31 22:36:36 +0900

    Session1

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,72 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: FractionallySizedBox(
+            widthFactor: 0.5,
+            child: Column(
+              children: [
+                const Spacer(),
+                Column(
+                  children: [
+                    const AspectRatio(
+                      aspectRatio: 1,
+                      child: Placeholder(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              '**℃',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge!
+                                  .copyWith(color: Colors.blue),
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              '**℃',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge!
+                                  .copyWith(color: Colors.red),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Flexible(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 80),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                              child: TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Close'))),
+                          Expanded(
+                              child: TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Reload'))),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
