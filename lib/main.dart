7544679077af0e6cfacc38ec1c802cb21edbbc33
okt4hei <142867353+okt4hei@users.noreply.github.com>
2024-08-20 22:17:26 +0900

    Session3

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_training/yumemi_api.dart';
+import 'package:flutter_training/pages/green.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  MainAppState createState() => MainAppState();
-}
-
-class MainAppState extends State<MainApp> {
-  final YumemiApi _yumemiApi = YumemiApi();
-  Widget weatherIcon = const Placeholder();
-
-  void reload() {
-    final weatherCondition = _yumemiApi.fetchWeather();
-    setState(() {
-      weatherIcon = SvgPicture.asset('assets/$weatherCondition.svg');
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.5,
-            child: Column(
-              children: [
-                const Spacer(),
-                Column(
-                  children: [
-                    AspectRatio(
-                      aspectRatio: 1,
-                      child: weatherIcon,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              '**℃',
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge!
-                                  .copyWith(color: Colors.blue),
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              '**℃',
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge!
-                                  .copyWith(color: Colors.red),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Flexible(
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 80),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextButton(
-                              onPressed: () {},
-                              child: const Text('Close'),
-                            ),
-                          ),
-                          Expanded(
-                            child: TextButton(
-                              onPressed: reload,
-                              child: const Text('Reload'),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: GreenPage(),
     );
   }
 }
